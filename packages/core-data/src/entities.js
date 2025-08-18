db5d1359953a465242e873b36317f8fdd1455d1a
chriszarate <chris.zarate@automattic.com>
2025-08-18 13:19:53 -0600

    Reform object

--- packages/core-data/src/entities.js
+++ packages/core-data/src/entities.js
@@ -337,8 +337,10 @@ async function loadPostTypeEntities() {
 				applyChangesToCRDTDoc: ( ydoc, allChanges ) => {
 					// local changes happened. Apply the differences to the ydoc
 					const ycontent = ydoc.getMap( 'document' );
-					const changes = Object.entries( allChanges ).filter(
-						( [ key ] ) => syncedProperties.has( key )
+					const changes = Object.fromEntries(
+						Object.entries( allChanges ).filter( ( [ key ] ) =>
+							syncedProperties.has( key )
+						)
 					);
 
 					Object.entries( changes ).forEach( ( [ key, value ] ) => {
